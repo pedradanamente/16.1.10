0eb8f8cc885eef5541d8c0c34c6d4621605c54c0
pedradanamente <rubyandremachado@gmail.com>
2016-01-08 20:39:18 -0200

    alteracoes pelo script automatico

---
+++ Especialização (Marcos Marques)/Modulo 1/script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir /home/vinicius/Desktop/alimentacao
+mkdir /home/vinicius/Desktop/alimentacao/cafe
+mkdir /home/vinicius/Desktop/alimentacao/cafe/pao
+mkdir /home/vinicius/Desktop/alimentacao/cafe/leite
+mkdir /home/vinicius/Desktop/alimentacao/cafe/queijo
+
+mkdir /home/vinicius/Desktop/alimentacao/almoco/
+mkdir /home/vinicius/Desktop/alimentacao/almoco/arroz
+mkdir /home/vinicius/Desktop/alimentacao/almoco/feijao
+mkdir /home/vinicius/Desktop/alimentacao/almoco/carne
+mkdir /home/vinicius/Desktop/alimentacao/almoco/suco
+
+mkdir /home/vinicius/Desktop/alimentacao/janta
+mkdir /home/vinicius/Desktop/alimentacao/janta/sopa
+mkdir /home/vinicius/Desktop/alimentacao/janta/sopa/legumes
+mkdir /home/vinicius/Desktop/alimentacao/janta/sopa/frango
+mkdir /home/vinicius/Desktop/alimentacao/janta/sopa/massa
+
+cd /home/vinicius/Desktop/alimentacao
+tree alimentacao
+
---
+++ HTML/Classe Conexao PHP + MySQLi_files/ca-pub-3727048618220700.js
@@ -0,0 +1,1 @@
+(google_async_config = window.google_async_config || {})['ca-pub-3727048618220700'] = {"sra_enabled":false};
